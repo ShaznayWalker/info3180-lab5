2138b420959c2faf7f1c71b909643f8545ea118f
Shaznay Walker <shaznay.walker@mymona.uwi.edu>
2026-04-13 12:53:35 -0500

    exercise 5 is completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 import AddMovieFormView from '../views/AddMovieFormView.vue'
+import MoviesView from '../views/MoviesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // lazy-loaded route
+      component: () => import('../views/AboutView.vue')
+    },
+    {
       path: '/movies/create',
       name: 'add-movie',
       component: AddMovieFormView
     },
-   
+    {
+      path: '/movies',
+      name: 'movies',
+      component: MoviesView
+    }
   ]
 })
 
